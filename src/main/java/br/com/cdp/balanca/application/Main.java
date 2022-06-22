9d594f743fc349cb1074b76f10e16bd770947c6d
Natanael R. da S. Carvalho <n2carvalho@cdp.com.br>
2022-06-22 09:39:04 -0300

    voltando ao original

--- src/main/java/br/com/cdp/balanca/application/Main.java
+++ src/main/java/br/com/cdp/balanca/application/Main.java
@@ -25,7 +25,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         stage = primaryStage;
         Parent fxmlLogin = FXMLLoader.load(Main.class.getResource("/br/com/cdp/balanca/view/login.fxml"));
-        loginScene = new Scene(fxmlLogin, 601, 400);
+        loginScene = new Scene(fxmlLogin, 600, 400);
 
         primaryStage.setTitle("Balança Rodoviária CDP");
         primaryStage.setScene(loginScene);
